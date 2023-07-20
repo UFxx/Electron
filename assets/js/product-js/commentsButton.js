c8f902e622b45c3ce74d15bd03b8a083f6610f17
Dmitriy <87441804+UFxx@users.noreply.github.com>
2023-07-21 01:07:55 +0300

    Bug fixed
    Validation of comments, selection of a product image by clicking

--- assets/js/product-js/commentsButton.js
+++ assets/js/product-js/commentsButton.js
@@ -1,26 +1,39 @@
 const form = document.querySelector('form');
 const nameInput = document.querySelector('#form-name');
+const label = document.querySelectorAll('label')
 const textInput = document.querySelector('#form-text');
 const ratingInput = document.querySelector('#form-rating');
 const publishButton = document.querySelector('#leave-a-comment__button');
 const usersComments = document.querySelector('.users-comments');
-const fields = document.querySelectorAll('.field')
 
-// Validation
-// publishButton.addEventListener('click', function (event) {
-//     event.preventDefault();
-//         for (i = 0; i < fields.length; i++) {
-//             if (!fields.value) {
-//                 fields[i].style.border = '1px solid red';
-//             } else {
-//                 fields[i].style.border = '';
-//                 publish();
-//             }
-//         }
-//     })
+// Rating input limit
+ratingInput.addEventListener('input', function () {
+if (ratingInput.value >  10) {
+    ratingInput.value = 10
+} else if (ratingInput.value < 0) {
+    ratingInput.value = 1
+}
+});
 
 // Add a comment
-publishButton.addEventListener('click', function () {
+publishButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    
+    // Validate
+    if (ratingInput.value === '' && nameInput.value === '') {
+        label.forEach(element => {
+            element.style.opacity = 100;
+        })
+        publishButton.setAttribute('disabled')
+    } else {
+        label.forEach(element => {
+            element.style.opacity = 0;
+    })
+
+        publishButton.removeAttribute('disabled');
+    }
+
+    // Create a comment
     const createDiv = document.createElement('div');
     createDiv.classList.add('users-comments__item');
     const createPName = document.createElement('p');
@@ -35,7 +48,7 @@ publishButton.addEventListener('click', function () {
     }
     createPName.textContent = nameInput.value;
     if (createPName.textContent === '') {
-        createPName.textContent = 'Имя Фамилия';
+        createPName.textContent = 'Имя не указано';
     };
     createPText.textContent = textInput.value;
     createRating.textContent = `Оценка: ${ratingInput.value}/10`;
@@ -48,11 +61,12 @@ publishButton.addEventListener('click', function () {
     textInput.value = '';
     ratingInput.value = '';
 
-    publishButton.textContent = 'Опубликовано!';
-    publishButton.style.border = '2px solid #08B251';
-    publishButton.setAttribute('disabled', '');
-    publishButton.style.backgroundColor = 'green';
+    // Publish successfully on button
+    this.textContent = 'Опубликовано!';
+    this.style.border = '2px solid #08B251';
+    this.setAttribute('disabled', '');
+    this.style.backgroundColor = 'green';
     setTimeout(()=>{
-        publishButton.style.backgroundColor = 'transparent';
+        this.style.backgroundColor = 'transparent';
     }, 1000)
 });
